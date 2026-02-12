0bb5cf117002df6288cc265239312fd2c7c1faa0
NHTLam <105333043+NHTLam@users.noreply.github.com>
2026-02-12 14:57:51 +0700

    Sửa lại theo ý mấy con đĩ

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -5,7 +5,6 @@ import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
-import EcommerceMetricsFirst from "../../components/ecommerce/EcommerceMetricsFirst";
 
 export default function Home() {
   return (
@@ -15,9 +14,6 @@ export default function Home() {
         description=""
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12">
-          <EcommerceMetricsFirst />
-        </div>
         <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
           <TopSellingProductsChart />
