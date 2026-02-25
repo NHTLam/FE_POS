c956009f62b236871ff7634548ef2f1e4d5ed0a8
NHTLam <105333043+NHTLam@users.noreply.github.com>
2026-02-25 17:10:35 +0700

    dev thêm 1 số thứ

--- src/pages/Dashboard/Sale.tsx
+++ src/pages/Dashboard/Sale.tsx
@@ -1,25 +1,32 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
+import SaleMetrics from "../../components/ecommerce/SaleMetrics";
+import OutOfStock from "../../components/ecommerce/OutOfStock";
+import InProgressOrder from "../../components/ecommerce/InProgressOrder";
+import WaittingForPaymentOrder from "../../components/ecommerce/WaittingForPaymentOrder";
 import PageMeta from "../../components/common/PageMeta";
+import TableSmallList from "../../components/ecommerce/TableSmallList";
 
 export default function Sale() {
   return (
     <>
       <PageMeta
-        title="POS"
+        title="Sale"
         description=""
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
-          <div className="flex gap-4 md:gap-6">
-            <RecentOrders />
-            <RecentOrders />
+        <div className="col-span-12 space-y-6 xl:col-span-8">
+          <SaleMetrics />
+          <div className="flex gap-4 md:gap-6 w-full">
+            <div className="flex-1">
+              <InProgressOrder />
+            </div>
+            <div className="flex-1">
+              <WaittingForPaymentOrder />
+            </div>
           </div>
         </div>
-        <div className="col-span-12 xl:col-span-5 space-y-6">
-          <RecentOrders />
-          <RecentOrders />
+        <div className="col-span-12 space-y-6 xl:col-span-4">
+          <TableSmallList />
+          <OutOfStock />
         </div>
       </div>
     </>
